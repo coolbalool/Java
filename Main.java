2baebf76f605d5ee8a57015c33cc2d29e883d490
Itamar <itamarca117@gmail.com>
2022-08-14 21:19:22 +0300

    init

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.io.File;
 import java.util.*;
-
+// test I like cheese
 public class Main
 {
 
     public static void main(String[] args)
     {
+
     // TicTacToe t = new TicTacToe(3);
     //  System.out.println(t.play());
  
