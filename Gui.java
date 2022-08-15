ddfc352195d270050869c2fb76c1b337df209899
Itamar <itamarca117@gmail.com>
2022-08-16 02:49:39 +0300

    init

--- Gui.java
+++ Gui.java
@@ -1,15 +1,18 @@
 import javax.swing.*;
 
+
 public class Gui {
 
     public Gui()
     {
         JFrame frame = new JFrame();    //creating instance of JFrame  
-        JButton button =new JButton("click");   //creating instance of JButton  
-        button.setBounds(130,100,100, 40);  //x axis, y axis, width, height  
-          
-        frame.add(button);//adding button in JFrame  
-          
+
+    JButton button =new JButton("Free VeBux");   //creating instance of JButton  
+    button.setBounds(130,100,200, 200);  //x axis, y axis, width, height  
+
+    JTextArea text = new JTextArea("Click here for free ram");
+
+    frame.add(button,text);//adding button in JFrame  
     frame.setSize(400,500);//400 width and 500 height  
     frame.setLayout(null);//using no layout managers  
     frame.setVisible(true);//making the frame visible  
