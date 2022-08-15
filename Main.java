49a548b02e9d528dfe03e29b3b3310c92c970583
Itamar <itamarca117@gmail.com>
2022-08-16 02:19:15 +0300

    init

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 import java.io.File;
 import java.util.*;
-// test I like cheese
+
 public class Main
 {
 
     public static void main(String[] args)
     {
 win
-    // TicTacToe t = new TicTacToe(3);
+     //TicTacToe t = new TicTacToe(3);
     //  System.out.println(t.play());
  
         Checkers c = new Checkers();
