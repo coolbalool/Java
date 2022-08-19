ae80db5b040dffc30d2e73d3a71a4ace3ab44088
Itamar <itamarca117@gmail.com>
2022-08-19 16:16:01 +0300

    init

--- Gui.java
+++ Gui.java
@@ -9,12 +9,14 @@ public class Gui implements ActionListener{
 
         private JFrame frame;
         private JPanel panel;
+        private JButton button;
         private JLabel userLabel;
         private JLabel passLabel;
+        private JLabel loginLabel;
         private JTextField userField;
         private JPasswordField passField;
-        private JButton button;
         private String password,username;
+        private CardLayout layout;
 
     public Gui()
     {
@@ -23,38 +25,49 @@ public class Gui implements ActionListener{
 
         frame = new JFrame();
         panel = new JPanel();
+
         frame.setSize(500,300);
         frame.setVisible(true);
         frame.setTitle("Gui");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
         frame.setVisible(true);
-
+        frame.add(panel);
         panel.setLayout(null);
         panel.setVisible(true);
-
-        userLabel = new JLabel("Username:");
-        userLabel.setBounds(10,20,70,50);
-        panel.add(userLabel);
-
-        userField = new JTextField(20);
-        userField.setBounds(80,35,120,20);
-        panel.add(userField);
-
-        passLabel = new JLabel("Password:");
-        passLabel.setBounds(10,50,70,50);
-        panel.add(passLabel);
-
-        passField = new JPasswordField(20);
-        passField.setBounds(80,65,120,20);
-        panel.add(passField);
-
-        button = new JButton("Login");
-        button.setBounds(80,100,70,30);
-        button.addActionListener(this);
-        panel.add(button);
+        loginPanel();
 
         panel.updateUI();
+        
+    }
+
+    private void loginPanel()
+    {
+
+      userLabel = new JLabel("Username:");
+      userLabel.setBounds(10,20,70,50);
+      panel.add(userLabel);
+
+      userField = new JTextField(20);
+      userField.setBounds(80,35,120,20);
+      panel.add(userField);
+
+      passLabel = new JLabel("Password:");
+      passLabel.setBounds(10,50,70,50);
+      panel.add(passLabel);
+
+      passField = new JPasswordField(20);
+      passField.setBounds(80,65,120,20);
+      panel.add(passField);
+
+      loginLabel = new JLabel("");
+      loginLabel.setBounds(80,130,200,20);
+      panel.add(loginLabel);
+
+      button = new JButton("Login");
+      button.setBounds(80,100,70,30);
+      button.addActionListener(this);
+      panel.add(button);
+      panel.updateUI();
     }
 
     @Override
@@ -64,7 +77,9 @@ public class Gui implements ActionListener{
               {
                 if (userField.getText().equals(username))
                 if (passField.getText().equals(password))
-                System.out.println("asd");
+                loginLabel.setText("Login successfully");
+                else loginLabel.setText("Username or password incorrect");
+                else loginLabel.setText("Username or password incorrect");
               }
     }
     
