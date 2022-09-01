0c74cdeb44fd5106a5befd15d012ba6b692e660c
Itamar <itamarca117@gmail.com>
2022-09-01 23:19:50 +0300

    init

--- Gui.java
+++ Gui.java
@@ -1,187 +1,246 @@
 
-import javax.swing.*;
+    import javax.swing.*;
 
-import javafx.event.ActionEvent;
+    import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
 import java.awt.event.ActionListener;
 
-import java.awt.*;
+    import java.awt.*;
 
-public class Gui implements ActionListener{
+    public class Gui implements ActionListener{
 
-        // login + register page 
-        private boolean isLoginMode = true,ranLogin = false; 
-        private JPanel loginPanel;
-        private JButton loginButton,registerButton;
-        private JTextField userField;
-        private JPasswordField passField;
-        private JLabel loginLabel,loginModeLabel;
-        private String password,username;
-        // ticTacToe
-        private JPanel tPanel;
-        private JButton tExit;
-        TicTacToe t;
+    // login + register page 
+    private boolean isLoginMode = true,ranLogin = false; 
+    private JPanel loginPanel;
+    private JButton loginButton,registerButton;
+    private JTextField userField;
+    private JPasswordField passField;
+    private JLabel loginLabel,loginModeLabel;
+    private String password,username;
+    // ticTacToe
+    private JPanel tPanel;
+    private JButton tExit;
+    private JButton[][] board;
+    private boolean tIsCross;
 
-        private JFrame frame;
-        private JPanel panel;
-        private CardLayout cl;
+
+    private JFrame frame;
+    private JPanel panel;
+    private CardLayout cl;
 
     public Gui()
     {
-        username = "dag";
-        password = "123daf";
+    username = "dag";
+    password = "123daf";
 
-        frame = new JFrame("GUI");
-        panel = new JPanel(cl);
-        
-        
-        cl = new CardLayout();
+    frame = new JFrame("GUI");
+    panel = new JPanel(cl);
 
-        frame.setSize(500,300);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.add(panel);
-        panel.setLayout(cl);
 
-        t = new TicTacToe();
-        loginPanel(true);
-        panel.add(loginPanel,"login");
-        panel.add(tPanel,"ttt");
-        cl.show(panel,"ttt");
-        System.out.println("d");
-        panel.updateUI();
+    cl = new CardLayout();
+
+    frame.setSize(500,300);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    frame.add(panel);
+    panel.setLayout(cl);
+
+    loginPanel(true);
+    ticTacToe(null);
+
+    panel.add(loginPanel,"login");
+    panel.add(tPanel,"ttt");
+    cl.show(panel,"ttt");
+    panel.updateUI();
     }
 
-   public class TicTacToe
-   {
-
-        private JButton[][] board;
-        
-        private boolean tIsCross;
-
-    private TicTacToe()
+    public void ticTacToe(String operation)
     {
-      tPanel = new JPanel(null);
-      tPanel.setBackground(Color.gray);
-      tExit = new JButton("Exit");
-      tExit.setBounds(400,100,75,50);
-      tPanel.add(tExit);
-      board = new JButton[3][3];
-      for (int i = 0; i < board.length; i++)      
-        for (int j = 0;  j < board.length; j++)
-        {
-          board[i][j] = new JButton(i + " " + j);
-          board[i][j].setBounds(j * 100, i * 100 ,60,60);
-          tPanel.add(board[i][j]);
-        }
-  }
-}
+
+      
+    if (operation == null)
+    {
+    tPanel = new JPanel(null);
+    tPanel.setBackground(Color.gray);
+
+    tExit = new JButton("Exit");
+    tExit.setBounds(400,100,75,50);
+    tExit.addActionListener(this);
+    tPanel.add(tExit);
+
+    JLabel playerTxt = new JLabel("X:");
+    playerTxt.setForeground(Color.white);
+    playerTxt.setBounds(400,50,70,70);
+    tPanel.add(playerTxt);
+    tIsCross = true;
     
+    board = new JButton[3][3];
+    for (int i = 0; i < board.length; i++)      
+    for (int j = 0;  j < board.length; j++)
+    {
+    board[i][j] = new JButton("");
+    board[i][j].setBounds(j * 100, i * 100 ,60,60);
+    board[i][j].addActionListener(this);
+    tPanel.add(board[i][j]);
+    
+    }
+    }
+
+    if(operation == "check")
+    {
+        for (int i = 0; i < board.length; i++)
+        {
+          f
+        }
+    }
+
+    }
+
     private void loginPanel(boolean changeTextMode)
     {
-        
-         JLabel userLabel;
-         JLabel passLabel;
-         
-         loginPanel = new JPanel(null);
-         loginPanel.setBackground(Color.gray);
 
-        if (changeTextMode && ranLogin) 
-        {
-          if (isLoginMode) 
-          loginModeLabel.setText("Login Page");
-          else 
-          loginModeLabel.setText("Register Page");
-          return;
-        }
+    JLabel userLabel;
+    JLabel passLabel;
 
-        else if (!ranLogin && changeTextMode)
-        {
-          loginPanel(false);
-          loginPanel(true);
-        }
+    loginPanel = new JPanel(null);
+    loginPanel.setBackground(Color.gray);
 
-      userLabel = new JLabel("Username:");
-      userLabel.setBounds(10,50,70,50);
-      userLabel.setForeground(Color.white);
-      loginPanel.add(userLabel);
+    if (changeTextMode && ranLogin) 
+    {
+    if (isLoginMode) 
+    loginModeLabel.setText("Login Page");
+    else 
+    loginModeLabel.setText("Register Page");
+    return;
+    }
 
-      userField = new JTextField(20);
-      userField.setBounds(80,65,170,20);
-      loginPanel.add(userField);
+    else if (!ranLogin && changeTextMode)
+    {
+    loginPanel(false);
+    loginPanel(true);
+    }
 
-      passLabel = new JLabel("Password:");
-      passLabel.setBounds(10,80,70,50);
-      passLabel.setForeground(Color.white);
-      loginPanel.add(passLabel);
+    userLabel = new JLabel("Username:");
+    userLabel.setBounds(10,50,70,50);
+    userLabel.setForeground(Color.white);
+    loginPanel.add(userLabel);
 
-      passField = new JPasswordField(20);
-      passField.setBounds(80,95,170,20);
-      loginPanel.add(passField);
+    userField = new JTextField(20);
+    userField.setBounds(80,65,170,20);
+    loginPanel.add(userField);
 
-      loginLabel = new JLabel("");
-      loginLabel.setBounds(80,160,200,20);
-      loginLabel.setForeground(Color.white);
-      loginPanel.add(loginLabel);
+    passLabel = new JLabel("Password:");
+    passLabel.setBounds(10,80,70,50);
+    passLabel.setForeground(Color.white);
+    loginPanel.add(passLabel);
 
-      loginButton = new JButton("Login");
-      loginButton.setBounds(80,130,70,30);
-      loginButton.addActionListener(this);
-      loginPanel.add(loginButton);
-      
+    passField = new JPasswordField(20);
+    passField.setBounds(80,95,170,20);
+    loginPanel.add(passField);
 
-      registerButton = new JButton("Register");
-      registerButton.setBounds(150,130,100,30);
-      registerButton.addActionListener(this);
-      loginPanel.add(registerButton);
+    loginLabel = new JLabel("");
+    loginLabel.setBounds(80,160,300,20);
+    loginLabel.setForeground(Color.white);
+    loginPanel.add(loginLabel);
 
-      loginModeLabel = new JLabel("Login Page");
-      loginModeLabel.setBounds(80,25,200,20);
-      loginModeLabel.setForeground(Color.white);
-      loginPanel.add(loginModeLabel);
+    loginButton = new JButton("Login");
+    loginButton.setBounds(80,130,70,30);
+    loginButton.addActionListener(this);
+    loginPanel.add(loginButton);
 
-      loginPanel.updateUI();
-      ranLogin = true;
+
+    registerButton = new JButton("Register");
+    registerButton.setBounds(150,130,100,30);
+    registerButton.addActionListener(this);
+    loginPanel.add(registerButton);
+
+    loginModeLabel = new JLabel("Login Page");
+    loginModeLabel.setBounds(80,25,200,20);
+    loginModeLabel.setForeground(Color.white);
+    loginPanel.add(loginModeLabel);
+
+    loginPanel.updateUI();
+    ranLogin = true;
     }
 
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) 
     {
-            if (e.getSource() == loginButton)
-              {
-                loginPanel(true);
-                if (userField.getText().equals(username))
-                if (passField.getText().equals(password))
-                loginLabel.setText("Login successfully");
-                else loginLabel.setText("Username or password incorrect");
-                else loginLabel.setText("Username or password incorrect");
-                
-              }
+    if (e.getSource() == loginButton)
+      {
+        loginPanel(true);
+        if (userField.getText().equals(username))
+        if (passField.getText().equals(password))
+        {
+        loginLabel.setText("Login successfully");
+        loginLabel.setForeground(Color.white);
+        }
+        else
+        {
+          loginLabel.setText("Username or password incorrect");
+          loginLabel.setForeground(Color.red);
+        } 
+        
+        else 
+        {
+          loginLabel.setText("Username or password incorrect");
+          loginLabel.setForeground(Color.red);
+        }
+        return;
+      }
 
-            if (e.getSource() == registerButton)
-            {
-              if (isLoginMode)
-              {
-                isLoginMode = false;
-                loginPanel(true);
-              }
-              else {
-                if (!userField.getText().equals("") && !passField.getText().equals(""))
-              {
-                username = userField.getText();
-                password = passField.getText();
-              }
-              else loginLabel.setText("Username or password cannot be blank");
-                isLoginMode = true;
-              }
-            }
-
-           if (e.getSource() == tExit)
-           {
-            System.out.println("d");
-           }
+    if (e.getSource() == registerButton)
+    {
+      if (isLoginMode)
+      {
+        isLoginMode = false;
+        loginLabel.setText("");
+        loginLabel.setForeground(Color.white);
+        loginPanel(true);
+      }
+      else {
+        if (!userField.getText().equals("") && !passField.getText().equals(""))
+      {
+        username = userField.getText();
+        password = passField.getText();
+      }
+      else
+      {
+        loginLabel.setText("Username or password cannot be blank");
+        loginLabel.setForeground(Color.white);
+      }
+        isLoginMode = true;
+      }
+      return;
     }
-    
-}
+
+    if (e.getSource() == tExit)
+    {
+      cl.show(panel,"login");
+      return;
+    }
+
+    for (int i = 0; i < board.length; i++)
+    for (int j = 0; j < board.length; j++)
+    {
+      if (e.getSource() == board[i][j])
+      {
+        if (board[i][j].getText() == "")
+        {
+        if (tIsCross) 
+        board[i][j].setText("X");
+        else board[i][j].setText("O");
+        tIsCross = !tIsCross;
+        ticTacToe("check");
+      }
+
+    }
+    }
+
+    }
+    }    
+
+
 
