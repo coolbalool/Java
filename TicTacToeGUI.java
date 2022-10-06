206cf77292ca0cf081a3c75f472fdd0f6afb09c3
Itamar <itamarca117@gmail.com>
2022-10-06 21:12:04 +0300

    init

--- TicTacToeGUI.java
+++ TicTacToeGUI.java
@@ -9,91 +9,129 @@ import java.awt.*;
 
     private JFrame frame;
     private JPanel panel;
-    private JButton board[][],tExit,tReset;
-    private JLabel tPlayerTxt,tWinnerText;
-    private boolean tIsCross;
-    private int tMoves;
-    private String tWinner;
+    private JButton board[][],exit,reset;
+    private JLabel playerTxt,winnerTxt;
+    private boolean isCross,gameOver;
+    private int moves;
     
-    
+    public static void main(String[] args)
+      {
+        new TicTacToeGUI();
+      }
+
+    private enum GameState
+      {
+
+      SPACE(" "),
+      XP("X"),
+      OP("O"); 
+
+        String state;
+        GameState(String state)
+        {
+          this.state = state;
+        }
+
+        public String text()
+        {
+          return state;
+        }
+
+      }
+
     public TicTacToeGUI()
     {
-
-    frame = new JFrame("Tic Tac Toe");
-    frame.setSize(500,300);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-    frame.add(panel);
-    ticTacToe(null);
+      setLayout();
     }
 
     public void setLayout()
     {
-        panel = new JPanel(null);
-        panel.setBackground(Color.gray);
+
+      // frame 
+      frame = new JFrame("Tic Tac Toe");
+      frame.setSize(500,300);
+      frame.setVisible(true);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+  
+      // panel
+      panel = new JPanel(null);
+      panel.setBackground(Color.gray);
+      frame.add(panel);
+
+        // exit button
+        exit = new JButton("Exit");
+        exit.setBounds(400,100,75,50);
+        exit.addActionListener(this);
+        panel.add(exit);
     
-        tExit = new JButton("Exit");
-        tExit.setBounds(400,100,75,50);
-        tExit.addActionListener(this);
-        panel.add(tExit);
+        // reset button
+        reset = new JButton("Reset");
+        reset.setBounds(400,150,75,50);
+        reset.addActionListener(this);
+        reset.setVisible(false);
+        panel.add(reset);
     
-        tReset = new JButton("Reset");
-        tReset.setBounds(400,150,75,50);
-        tReset.addActionListener(this);
-        tReset.setVisible(false);
-        panel.add(tReset);
+        // player turn txt
+        playerTxt = new JLabel(GameState.XP.text() + ":");
+        playerTxt.setForeground(Color.white);
+        playerTxt.setBounds(400,50,70,70);
+        panel.add(playerTxt);
     
-        tPlayerTxt = new JLabel("X:");
-        tPlayerTxt.setForeground(Color.white);
-        tPlayerTxt.setBounds(400,50,70,70);
-        panel.add(tPlayerTxt);
+        // winner txt
+        winnerTxt = new JLabel("");
+        winnerTxt.setForeground(Color.white);
+        winnerTxt.setBounds(310,130,130,70);
+        panel.add(winnerTxt);
     
-        tWinnerText = new JLabel("");
-        tWinnerText.setForeground(Color.white);
-        tWinnerText.setBounds(310,130,130,70);
-        panel.add(tWinnerText);
-    
-        tIsCross = true;
-        tMoves = 0;
-        tWinner = "";
+        // board + start things
+        isCross = true;
+        gameOver = false;
+        moves = 0;
         board = new JButton[3][3];
         for (int i = 0; i < board.length; i++)      
         for (int j = 0;  j < board.length; j++)
         {
-        board[i][j] = new JButton("");
+        board[i][j] = new JButton(GameState.SPACE.text());
         board[i][j].setBounds(j * 100, i * 100 ,60,60);
         board[i][j].addActionListener(this);
         panel.add(board[i][j]);
         }
+
+        // refresh panel
+        panel.updateUI();
     }
 
     public void checkWinner()
     {
+
+      // checks if the move count is too small for a win
+      if (moves < 4) return;
+
         // tie checker
-      if (tMoves >= 9) 
+      if (moves >= 9) 
       {
-        tWinner = "tie";
-        ticTacToe("go");
+        winHandler(GameState.SPACE);
         return;
       }
 
-      // checks if the move count is too small for a win
-      if (tMoves < 4) return;
- 
       String temp = "";
         for (int i = 0; i < board.length; i++)
         {
           // row
-          if (board[i][0].getText() == "") continue;
+          if (board[i][0].getText() == GameState.SPACE.text()) continue;
           temp = board[i][0].getText();
           for(int j = 0;  j < board.length; j++)
           {
               if (!temp.equals(board[i][j].getText())) break;
               if (j == board.length -1) 
               {
-                tWinner = temp;
-                ticTacToe("go");
+
+                if(temp.equals(GameState.XP.text()))
+                winHandler(GameState.XP);
+
+                if(temp.equals(GameState.OP.text()))
+                winHandler(GameState.OP);
                 return;
               }
             }
@@ -102,112 +140,116 @@ import java.awt.*;
           for (int i = 0; i < board.length; i++)  
           {
             // col
-            if (board[0][i].getText() == "") continue;
+            if (board[0][i].getText() == GameState.SPACE.text()) continue;
             temp = board[0][i].getText();
             for(int j = 0;  j < board.length; j++)
             {
                 if (!temp.equals(board[j][i].getText())) break;
                 if (j == board.length -1) 
                 {
-                  tWinner = temp;
-                  ticTacToe("go");
+                  if(temp.equals(GameState.XP.text()))
+                winHandler(GameState.XP);
+
+                if(temp.equals(GameState.OP.text()))
+                winHandler(GameState.OP);
+  
                   return;
                 }
               }
             }
 
         // (0,0) mid
-        if (board[0][0].getText() == "") return;
+        if (board[0][0].getText() == GameState.SPACE.text()) return;
         temp = board[0][0].getText();
         for (int i = 0;  i < board.length; i++)
         {
         if (!temp.equals(board[i][i].getText())) break;
         if (i == board.length -1) 
         {
-          tWinner = temp;
-          ticTacToe("go");
+          if(temp.equals(GameState.XP.text()))
+                winHandler(GameState.XP);
+
+                if(temp.equals(GameState.OP.text()))
+                winHandler(GameState.OP);
+
           return;
         }
         }
 
         // other mid
-        if (board[0][board.length -1].getText() == "") return;
+        if (board[0][board.length -1].getText() == GameState.SPACE.text()) return;
         temp = board[0][board.length -1].getText();
         for (int i = 0;  i < board.length; i++)
         {
         if (!temp.equals(board[i][board.length -i -1].getText())) break;
         if (i == board.length -1) 
         {
-          tWinner = temp;
-          ticTacToe("go");
+               if(temp.equals(GameState.XP.text()))
+                winHandler(GameState.XP);
+
+                if(temp.equals(GameState.OP.text()))
+                winHandler(GameState.OP);
+
+                return;
         }
       }
     }
 
-    public void winHandler()
+    public void winHandler(GameState state)
     {
-        
-    }
-
-    public void ticTacToe(String operation)
-    {
-
-    if (operation == "go")
-    {
-      if (tWinner == "tie")
+      if (state == GameState.SPACE)
       {
-        tWinnerText.setText("Game is a tie!");  
-        tPlayerTxt.setText("");
+        winnerTxt.setText("Game is a tie!");  
+        playerTxt.setText("");
       }
 
-      else if (tWinner == "X" || tWinner == "O")
+      else
       {
-        tWinnerText.setText(tWinner + " is the winner!");
-        tPlayerTxt.setText("");
+        winnerTxt.setText(state.text() + " is the winner!");
+        playerTxt.setText("");
       }
 
-      tReset.setVisible(true);
+      gameOver = true;
+      reset.setVisible(true);
     }
-    }
-
-    
+  
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) 
     {
    
-    if (e.getSource() == tExit)
+    if (e.getSource() == exit)
     {
       frame.dispose();
       return;
     }
 
-    if (e.getSource() == tReset)
+    if (e.getSource() == reset)
     {
       frame.dispose();
-      new Gui();
+      setLayout();
     }
 
     for (int i = 0; i < board.length; i++)
     for (int j = 0; j < board.length; j++)
     {
-      if (e.getSource() == board[i][j])
+      if (e.getSource() == board[i][j] && !gameOver)
       {
-        if (board[i][j].getText() == "")
+        if (board[i][j].getText() == GameState.SPACE.text())
         {
-        if (tIsCross) 
+        if (isCross) 
         {
-          board[i][j].setText("X");
-          tPlayerTxt.setText("O:");
+          board[i][j].setText(GameState.XP.text());
+          playerTxt.setText(GameState.OP.text() + ":");
         }
         
         else 
         {
-          board[i][j].setText("O");
-          tPlayerTxt.setText("X:");
+          board[i][j].setText(GameState.OP.text());
+          playerTxt.setText(GameState.XP.text() + ":");
         }
-        tIsCross = !tIsCross;
-        tMoves++;
-        ticTacToe("check");
+        isCross = !isCross;
+        moves++;
+        checkWinner();
       }
 
     }
