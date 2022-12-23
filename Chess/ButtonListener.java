be9242b24a4f243ff1b9b6a38f06b4a645200687
itamar <itamarca117@gmail.com>
2022-12-23 20:29:45 +0200

    init

---
+++ Chess/ButtonListener.java
@@ -0,0 +1,17 @@
+package Chess;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonListener implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        
+        
+        
+    }
+    
+}
